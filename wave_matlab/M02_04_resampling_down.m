9c1f76e01cc311ebeb232fcc9cec8a1a71dbab7a
muly <sssfeather@gmail.com>
2016-12-14 14:38:46 +0800

    ""

--- wave_matlab/M02_04_resampling_down.m
+++ wave_matlab/M02_04_resampling_down.m
@@ -1,91 +1,87 @@
 %%向下重采样
-clear all
-t0=0.01;%原采样间隔
-Fs0=1/t0;
-f0=Fs0/2; %原截止频率=1/2采样率
-%t1=0.02;%新采样间隔
-t1=0.02
-Fs1=1/t1;%新采样率
-ratio=t1/t0   %新采样间隔/原采样间隔
-N0=4096;%原数据点数
-N1=floor(N0*t0/t1)
-n=0:N0-1;
-t=n/Fs0;
-zeta=Fs1/(2*f0)  % 重采样的低通滤波器参数
-%zeta=1
-n1=0:N1-1;t1=n1/Fs1;
-xn0=sin(2*pi*1*t)+0.5*sin(2*pi*30*t);
-xn1=sin(2*pi*1*t1)+0.5*sin(2*pi*30*t1);
-%xn0=sin(2*pi*0.1*t);
-
+clearvars
+orgInv      = 0.01;                            %原采样间隔（每秒100点）
+newInv      = 0.02;                            %新采样间隔（每秒50点）
+orgDatLen   = 4096;                            %原数据点数
+newDatLen   = floor(orgDatLen*orgInv/newInv);  %新数据点数
+orgFs       = 1/orgInv;                        %原采样率
+newFs       = 1/newInv;                        %新采样率
+orgCutFre   = orgFs/2;                         %原截止频率=1/2采样率
+ratio       = newInv/orgInv;                   %新采样间隔/原采样间隔
+zeta        = newFs/(2*orgCutFre);             %重采样的低通滤波器参数
+%--------------------------------------------------------------------------
 figure;
-Nf=100; 
-plot(t(1+1:N0-Nf),xn0(1+Nf:N0-1),'r');hold on
-% 重采样+低通滤波
-xlabel('时间/s')
-for m=floor(Nf/ratio)+Nf+1:N1-floor(Nf/ratio)-1
-    x(m)=0;
-    for n=floor(m*ratio)-Nf:1:floor(m*ratio)+Nf
-        x(m)=x(m)+zeta*xn0(n)*sinc(zeta*(m*ratio-n));
+%--绘制采样率为1000和100的波形-------------------------------------------------
+t1000=0:0.001:orgDatLen*orgInv-1;
+mix1000=sin(2*pi*1*(t1000+1-orgInv))+0.5*sin(2*pi*30*(t1000+1-orgInv));
+plot(t1000,mix1000,'g')
+hold on
+
+t100=(0:orgDatLen-1)/orgFs;
+mix100=sin(2*pi*1*t100)+0.5*sin(2*pi*30*t100);
+plot(t100(1+1:orgDatLen-orgFs),mix100(1+orgFs:orgDatLen-1),'r');
+hold on
+
+%--向下采样为50采样率---------------------------------------------------------
+% Sinc是一个滤波器，它除去给定带宽之上的信号分量而只保留低频信号的理想电子滤波器。
+% 理想的Sinc滤波器（矩形滤波器）,有无限的延迟，现实世界中的滤波器只能是它的一个近似。
+comSta = floor(orgFs/ratio)+orgFs+1;
+comEnd = newDatLen-floor(orgFs/ratio)-1;
+difDat = zeros(1,comEnd);                     %初始化重采样序列
+for m=comSta:comEnd
+    for n=floor(m*ratio)-orgFs:1:floor(m*ratio)+orgFs
+        difDat(m)=difDat(m)+zeta*mix100(n)*sinc(zeta*(m*ratio-n));
     end
 end
 
-xl=length(x)
+t50=(0:newDatLen-1)/newFs;
+%???为什么乘重采样的低通滤波器参数（zeta）???
+%plot(tt1(floor(Fs0/ratio)+Fs0+1-Fs1+1:N1-floor(Fs0/ratio)-1-Fs1+1),x(floor(Fs0/ratio)+Fs0+1:N1-floor(Fs0/ratio)-1)*zeta,'b');
+%hold on
+plot(t50(orgFs+1:newDatLen-2*newFs),difDat(newFs+orgFs:newDatLen-newFs-1),'b');
+hold on
+xlabel('时间/s','FontName','SimSun')
 
-n1=0:N1-1;tt1=n1/Fs1;
-nn1=length(n1);
-ntt1=length(tt1);
-nx=length(x);
-%蓝线是向下重采样的结果.在重采样的同时已实现了低通滤波
-plot(tt1(floor(Nf/ratio)+Nf+1-Fs1+1:N1-floor(Nf/ratio)-1-Fs1+1),x(floor(Nf/ratio)+Nf+1:N1-floor(Nf/ratio)-1)*zeta,'b');hold on
-plot(tt1(floor(Nf/ratio)+Nf+1-Fs1+1:N1-floor(Nf/ratio)-1-Fs1+1),x(floor(Nf/ratio)+Nf+1:N1-floor(Nf/ratio)-1),'b');hold on
-  
-ty=0:0.001:4096*t0-Nf/Fs0;
-z=sin(2*pi*1*(ty+Nf/Fs0-0.01))+0.5*sin(2*pi*30*(ty+Nf/Fs0-0.01));
-%z=sin(2*pi*0.1*(ty+Nf/Fs0))
-plot(ty,z,'g')
-xn0fft=abs(fft(xn0,N0));
-n=0:1:(N0-1);
-f=n*Fs0/N0;
-
+%--计算不同采样率波形频谱------------------------------------------------------
 figure;
+fft100=abs(fft(mix100,orgDatLen));
+f=(0:(orgDatLen-1))*orgFs/orgDatLen;
 subplot(4,1,1);
-plot(f,xn0fft*2/N0);
-title('(a)   100sps采样的频谱')
-zfft=abs(fft(z,32768));
-n=0:1:(32768-1);
-fz=n/(32768*0.001);
-subplot(4,1,2);
-plot(fz,2*zfft/32768);title('(b)   1000sps采样的频谱');
+plot(f,fft100*2/orgDatLen);
+title('(a) 100sps    采样的频谱','FontName','SimSun')
 
-Nx=length(x);
+%???为什么使用32768（4096×8)???
+fft1000=abs(fft(mix1000,32768));
+fz=(0:(32768-1))/(32768*0.001);
+subplot(4,1,2);
+plot(fz,2*fft1000/32768);
+title('(b) 1000sps   采样的频谱','FontName','SimSun');
+
+Nx=length(difDat);
+xn1=sin(2*pi*1*t50)+0.5*sin(2*pi*30*t50);
 subplot(4,1,3);
-xn1fft=abs(fft(xn1));
-fxn1=n1*Fs1/N1;
-plot(fxn1,xn1fft*2/Nx);
-title('(c)    50sps采样未滤波频谱');
+fft50=abs(fft(xn1));
+fxn1=((0:newDatLen-1))*newFs/newDatLen;
+plot(fxn1,fft50*2/Nx);
+title('(c) 50sps 采样未滤波频谱','FontName','SimSun');
 
 subplot(4,1,4)
-xfft=abs(fft(x));  
-n=0:1:Nx-1;
-fx=n*Fs1/Nx;
+xfft=abs(fft(difDat));  
+fx=(0:Nx-1)*newFs/Nx;
 plot(fx,2*xfft/Nx);
-xlabel('频率/Hz');
-title('(d)    50sps采样，在重采样的同时已实现了低通滤波');
+title('(d) 50sps采样(在重采样的同时已实现了低通滤波)','FontName','SimSun');
+xlabel('频率/Hz','FontName','SimSun');
 
+%--用样条函数（spline)模拟信号恢复----------------------------------------------
 figure;
-%用样条函数（spline)模拟信号恢复
-%Dt=0.00005;t=-0.005:Dt:0.005;
 subplot(2,1,1)
-plot(t(1:100),xn0(1:100));
-title('x=sin(2*pi*1*t)+0.5*sin(2*pi*30*t),采样率100sps');
+plot(t100(1:100),mix100(1:100));
+title('x=sin(2*pi*1*t)+0.5*sin(2*pi*30*t),采样率100sps','FontName','SimSun');
+
 subplot(2,1,2)
-ty1=0:0.001:4096*t0;
-n=0:N0-1;t=n/Fs0;
-nty1=length(ty1);
-nxn0=length(xn0);
-nnt0=length(n*t0);
-xa=spline(n*t0,xn0,ty1);
+ty1=0:0.001:orgDatLen*orgInv;
+n=0:orgDatLen-1;
+xa=spline(n*orgInv,mix100,ty1);
 plot(ty1(1:1000),xa(1:1000))
-title('用样条函数插值由x恢复xa');
-xlabel('时间/s ');
+title('用样条函数插值由x恢复xa','FontName','SimSun');
+xlabel('时间/s','FontName','SimSun');

16ac4bd029706b606b0f14e9272331078babff99
muly <sssfeather@gmail.com>
2016-12-15 16:13:38 +0800

    ""

--- wave_matlab/m/M02_05_fft_abs.m
+++ wave_matlab/m/M02_05_fft_abs.m
@@ -2,103 +2,115 @@
 %按时间窗归一化的傅里叶谱振幅绝对值、
 %傅里叶振幅谱密度绝对值
 %傅里叶谱的真振幅
-%第一类信号：持续的周期信号
 
 clearvars
-%--产生x(t)=3*sin(2*pi*t)+1.5*sin(2*pi*5*t)+2信号----------------------------
-inv=0.02;
-shoLen=1000;
-shoSeq=inv*(1:shoLen);
-shoDat=4*sin(2*pi*shoSeq)+2*sin(2*pi*5*shoSeq)+2;
-lonLen=4000;
-lonSeq=inv*(1:lonLen);
-lonDat=4*sin(2*pi*lonSeq)+2*sin(2*pi*5*lonSeq)+2;
-length([shoSeq(1:shoLen),lonSeq(1:lonLen)])
-%------------------------------------
+%--使用持续的周期信号x(t)=3*sin(2*pi*t)+1.5*sin(2*pi*5*t)+2--------------------
+inv         = 0.02;                             %采样频率
+shoLen      = 1000;                             %短数据长度
+shoSeq      = inv*(1:shoLen);
+shoDat      = 4*sin(2*pi*shoSeq)+2*sin(2*pi*5*shoSeq)+2;   %???为什么不使用3*sin(2*pi*t)+1.5*sin(2*pi*5*t)+2???
+lonLen      = 4000;                             %长数据长度
+lonSeq      = inv*(1:lonLen);
+lonDat      = 4*sin(2*pi*lonSeq)+2*sin(2*pi*5*lonSeq)+2;
+%--绘制生成波形-------------------------
 figure
-plot(lonSeq(1:lonLen),lonDat(1:lonLen),'b')
-hold on
+%???按书中说前1000个点，后面4000个点，（1000+4000）× 0.02=100s，不应该是80s的时间长,第二章P20???
+
+%长80s绘图
+% plot(lonSeq(1:lonLen),lonDat(1:lonLen),'b')
+% hold on
+% plot(shoSeq(1:shoLen),shoDat(1:shoLen),'g')
+% hold on
+% plot([inv*shoLen,inv*shoLen],[max([shoDat,lonDat]),min([shoDat,lonDat])],'r')
+%长100s绘图
 plot(shoSeq(1:shoLen),shoDat(1:shoLen),'g')
+hold on
+plot(lonSeq(1:lonLen)+max(shoSeq),lonDat(1:lonLen),'b')
 hold on
 plot([inv*shoLen,inv*shoLen],[max([shoDat,lonDat]),min([shoDat,lonDat])],'r')
 xlabel('t/s')
 title('(a) test signal:x=4*sin(2*pi*t)+2*sin(2*pi*5*t)+2')
 
-%--计算离散傅里叶变换系数绝对值--------------------------------------------------
+%------------------------------------
+shoDf=1/(shoLen*inv);                   %???为什么横坐标这么计算???
+shoF=shoDf*(1:shoLen)-shoDf;
+lonDf=1/(lonLen*inv);
+lonF=lonDf*(1:lonLen)-lonDf;
+
+%--计算变换系数绝对值--------------------
 shoFft=fft(shoDat);
 shoDftAbs=abs(shoFft);
-shoDftAbs(1)=shoDftAbs(1)/2;
+shoDftAbs(1)=shoDftAbs(1)/2;            %???为什么第一个值要取一半值???
 lonFft=fft(lonDat);
 lonDftAbs=abs(lonFft);
 lonDftAbs(1)=lonDftAbs(1)/2;
+%--计算谱振幅绝对值----------------------
+shoFnAbs=abs(shoFft/shoLen);
+shoFnAbs(1)=shoFnAbs(1)/2;              
+lonFnAbs=abs(lonFft/lonLen);
+lonFnAbs(1)=shoFnAbs(1)/2;
+%--计算振幅谱密度绝对值-------------------
+shofndAbs=abs(inv*shoFft);
+shofndAbs(1)=shofndAbs(1)/2;
+lonFndAbs=abs(inv*lonFft);
+lonFndAbs(1)=lonFndAbs(1)/2;
+%--计算谱真实振幅------------------------
+shoReaAmp=2*abs(shoFft)/shoLen;
+shoReaAmp(1)=shoReaAmp(1)/2;
+lonReaAmp=2*abs(lonFft)/lonLen;
+lonReaAmp(1)=lonReaAmp(1)/2;
 
-fnAbs=abs(shoFft/shoLen);
-fnAbs(1)=fnAbs(1)/2;
-fndAbs=abs(inv*shoFft);
-fndAbs(1)=fndAbs(1)/2;
-
-an=2*abs(shoFft)/shoLen;
-an(1)=an(1)/2;
-df=1/(shoLen*inv);
-f=df*(1:shoLen)-df;
-df1=1/(lonLen*inv);
-f1=df1*(1:lonLen)-df1;
-%------------------------------------
+%--绘制计算结果--------------------------
 figure
-subplot(221)
-bar(f(1:shoLen),shoDftAbs(1:shoLen),2,'b')
-axis([-1 df*shoLen 0 10000])
+subplot(421)
+bar(shoF(1:shoLen),shoDftAbs(1:shoLen),2,'b')
+axis([-1 shoDf*shoLen 0 10000])
 title('(b) 变换系数绝对值','FontName','SimSun')
 xlabel('f/Hz')
-%------------------------------------
-subplot(222)
-bar(f1(1:lonLen),lonDftAbs(1:lonLen),8,'b')
-axis([-1 df1*lonLen 0 10000])
+
+subplot(422)
+bar(lonF(1:lonLen),lonDftAbs(1:lonLen),8,'b')
+axis([-1 lonDf*lonLen 0 10000])
 title('(b) 变换系数绝对值','FontName','SimSun')
 xlabel('f/Hz')
-%-------------------------------------
-fn_abs1=abs(lonFft/lonLen);
-fn_abs1(1)=fn_abs1(1)/2;
-subplot(223)
-bar(f(1:shoLen),fnAbs(1:shoLen),2,'b')
-axis([-1 df*shoLen 0 4])
+
+subplot(423)
+bar(shoF(1:shoLen),shoFnAbs(1:shoLen),2,'b')
+axis([-1 shoDf*shoLen 0 4])
 title('(c) 谱振幅绝对值','FontName','SimSun')
 xlabel('f/Hz')
-%-------------------------------------
-subplot(224)
-bar(f1(1:lonLen),fn_abs1(1:lonLen),8,'b')
-axis([-1 df1*lonLen 0 4])
+
+subplot(424)
+bar(lonF(1:lonLen),lonFnAbs(1:lonLen),8,'b')
+axis([-1 lonDf*lonLen 0 4])
 title('(c) 谱振幅绝对值','FontName','SimSun')
 xlabel('f/Hz')
-%--------------------------------------------------------------------------
-figure
-fnd_abs1=abs(inv*lonFft);
-fnd_abs1(1)=fnd_abs1(1)/2;
-subplot(221)
-bar(f(1:shoLen),fndAbs(1:shoLen),2,'b')
-axis([-1 df*shoLen 0 200])
+
+subplot(425)
+bar(shoF(1:shoLen),shofndAbs(1:shoLen),2,'b')
+axis([-1 shoDf*shoLen 0 200])
 title('(d) 振幅谱密度绝对值','FontName','SimSun')
 xlabel('f/Hz')
-subplot(222)
-bar(f1(1:lonLen),fnd_abs1(1:lonLen),8,'b')
-axis([-1 df1*lonLen 0 200])
+
+subplot(426)
+bar(lonF(1:lonLen),lonFndAbs(1:lonLen),8,'b')
+axis([-1 lonDf*lonLen 0 200])
 title('(d) 振幅谱密度绝对值','FontName','SimSun')
 xlabel('f/Hz')
-an1=2*abs(lonFft)/lonLen;
-an1(1)=an1(1)/2;
-subplot(223)
-bar(f(1:shoLen),an(1:shoLen),2,'b')
-axis([-1 df*shoLen 0 5])
-title('(e) 谱真实振幅','FontName','SimSun')
-xlabel('f/Hz')
-subplot(224)
-bar(f1(1:lonLen),an1(1:lonLen),8,'b')
-axis([-1 df1*lonLen 0 5])
+
+subplot(427)
+bar(shoF(1:shoLen),shoReaAmp(1:shoLen),2,'b')
+axis([-1 shoDf*shoLen 0 5])
 title('(e) 谱真实振幅','FontName','SimSun')
 xlabel('f/Hz')
 
-%--------------------------------------------------------------------------
-%使用地震记录波形
+subplot(428)
+bar(lonF(1:lonLen),lonReaAmp(1:lonLen),8,'b')
+axis([-1 lonDf*lonLen 0 5])
+title('(e) 谱真实振幅','FontName','SimSun')
+xlabel('f/Hz')
+
+%--使用地震记录波形-----------------------------------------------------------
 lonLen=16000;
 shoLen=8000;
 load M02_04_dalianE1.txt; % 读入一道波形数据
@@ -106,20 +118,20 @@ lonDat(1:lonLen)=M02_04_dalianE1(2501:lonLen+2500);
 shoDat(1:shoLen)=M02_04_dalianE1(2501:shoLen+2500);
 inv=0.02;
 shoSeq=inv*(1:shoLen);
-df=1/(shoLen*inv);
-f=df*(1:shoLen)-df;
+shoDf=1/(shoLen*inv);
+shoF=shoDf*(1:shoLen)-shoDf;
 shoFft=fft(shoDat);
 shoDftAbs=abs(shoFft);
 shoDftAbs(1)=shoDftAbs(1)/2;
-fnAbs=abs(shoFft/shoLen);
-fnAbs(1)=fnAbs(1)/2;
-fndAbs=abs(inv*shoFft);
-fndAbs(1)=fndAbs(1)/2;
-an=2*abs(shoFft)/shoLen;
-an(1)=an(1)/2;
+shoFnAbs=abs(shoFft/shoLen);
+shoFnAbs(1)=shoFnAbs(1)/2;
+shofndAbs=abs(inv*shoFft);
+shofndAbs(1)=shofndAbs(1)/2;
+shoReaAmp=2*abs(shoFft)/shoLen;
+shoReaAmp(1)=shoReaAmp(1)/2;
 lonSeq=inv*(1:lonLen);
-df1=1/(lonLen*inv);
-f1=df1*(1:lonLen)-df1;
+lonDf=1/(lonLen*inv);
+lonF=lonDf*(1:lonLen)-lonDf;
 %--------------------------------------------------------------------------
 figure
 plot(lonSeq(1:lonLen),lonDat(1:lonLen))
@@ -131,53 +143,53 @@ lonFft=fft(lonDat);
 lonDftAbs=abs(lonFft);
 lonDftAbs(1)=lonDftAbs(1)/2;
 subplot(221)
-bar(f(1:shoLen),shoDftAbs(1:shoLen),2)
+bar(shoF(1:shoLen),shoDftAbs(1:shoLen),2)
 %axis([-1 df*N 0 5000])
-axis([-1 df*shoLen 0 8000000])
+axis([-1 shoDf*shoLen 0 8000000])
 title('(b) dft-abs')
 xlabel('f/Hz')
 %------------------------------------
 subplot(222)
-bar(f1(1:lonLen),lonDftAbs(1:lonLen),2)
-axis([-1 df1*lonLen 0 8000000])
+bar(lonF(1:lonLen),lonDftAbs(1:lonLen),2)
+axis([-1 lonDf*lonLen 0 8000000])
 title('(b) dft-abs')
 xlabel('f/Hz')
 %-------------------------------------
-fn_abs1=abs(lonFft/lonLen);
-fn_abs1(1)=fn_abs1(1)/2;
+shoFnAbs=abs(lonFft/lonLen);
+shoFnAbs(1)=shoFnAbs(1)/2;
 subplot(223)
-bar(f(1:shoLen),fnAbs(1:shoLen),2)
-axis([-1 df*shoLen 0 1200])
+bar(shoF(1:shoLen),shoFnAbs(1:shoLen),2)
+axis([-1 shoDf*shoLen 0 1200])
 title('(c) fn-abs')
 xlabel('f/Hz')
 subplot(224)
-bar(f1(1:lonLen),fn_abs1(1:lonLen),2)
-axis([-1 df1*lonLen 0 1200])
+bar(lonF(1:lonLen),shoFnAbs(1:lonLen),2)
+axis([-1 lonDf*lonLen 0 1200])
 title('(c) fn-abs')
 xlabel('f/Hz')
 %--------------------------------------------------------------------------
 figure
-fnd_abs1=abs(inv*lonFft);
-fnd_abs1(1)=fnd_abs1(1)/2;
+lonFndAbs=abs(inv*lonFft);
+lonFndAbs(1)=lonFndAbs(1)/2;
 subplot(221)
-bar(f(1:shoLen),fndAbs(1:shoLen),2)
-axis([-1 df*shoLen 0 200000])
+bar(shoF(1:shoLen),shofndAbs(1:shoLen),2)
+axis([-1 shoDf*shoLen 0 200000])
 title('(d) fnd-abs')
 xlabel('f/Hz')
 subplot(222)
-bar(f1(1:lonLen),fnd_abs1(1:lonLen),2)
-axis([-1 df1*lonLen 0 200000])
+bar(lonF(1:lonLen),lonFndAbs(1:lonLen),2)
+axis([-1 lonDf*lonLen 0 200000])
 title('(d) fnd-abs')
 xlabel('f/Hz')
-an1=2*abs(lonFft)/lonLen;
-an1(1)=an1(1)/2;
+lonReaAmp=2*abs(lonFft)/lonLen;
+lonReaAmp(1)=lonReaAmp(1)/2;
 subplot(223)
-bar(f(1:shoLen),an(1:shoLen),2)
-axis([-1 df*shoLen 0 1500])
+bar(shoF(1:shoLen),shoReaAmp(1:shoLen),2)
+axis([-1 shoDf*shoLen 0 1500])
 title('(e) An')
 xlabel('f/Hz')
 subplot(224)
-bar(f1(1:lonLen),an1(1:lonLen),2)
-axis([-1 df1*lonLen 0 1500])
+bar(lonF(1:lonLen),lonReaAmp(1:lonLen),2)
+axis([-1 lonDf*lonLen 0 1500])
 title('(e) An')
 xlabel('f/Hz')

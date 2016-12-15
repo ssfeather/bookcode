51703e3540103376766a669c15b97be99e6da832
muly <sssfeather@gmail.com>
2016-12-15 09:04:57 +0800

    ""

--- wave_matlab/m/M02_05_fft_abs.m
+++ wave_matlab/m/M02_05_fft_abs.m
@@ -2,8 +2,8 @@
 %按时间窗归一化的傅里叶谱振幅绝对值、
 %傅里叶振幅谱密度绝对值
 %傅里叶谱的真振幅
-% 第一类信号：持续的周期信号
-% x(t)=3*sin(2*pi*t)+1.5*sin(2*pi*5*t)+2
+%第一类信号：持续的周期信号
+%x(t)=3*sin(2*pi*t)+1.5*sin(2*pi*5*t)+2
 %dt=0.01s,N=1024,t=dt*(1:N)
 clearvars
 dt=0.02;
@@ -13,12 +13,12 @@ df=1/(N*dt);
 f=df*(1:N)-df;
 x=4*sin(2*pi*t)+2*sin(2*pi*5*t)+2;
 y=fft(x);
-dft_abs=abs(y);
-dft_abs(1)=dft_abs(1)/2;
-fn_abs=abs(y/N);
-fn_abs(1)=fn_abs(1)/2;
-fnd_abs=abs(dt*y);
-fnd_abs(1)=fnd_abs(1)/2;
+dftAbs=abs(y);
+dftAbs(1)=dftAbs(1)/2;
+fnAbs=abs(y/N);
+fnAbs(1)=fnAbs(1)/2;
+fndAbs=abs(dt*y);
+fndAbs(1)=fndAbs(1)/2;
 an=2*abs(y)/N;
 an(1)=an(1)/2;
 N1=4000;
@@ -37,7 +37,7 @@ y1=fft(x1);
 dft_abs1=abs(y1);
 dft_abs1(1)=dft_abs1(1)/2;
 subplot(221)
-bar(f(1:N),dft_abs(1:N),0.2)
+bar(f(1:N),dftAbs(1:N),0.2)
 axis([-1 df*N 0 10000])
 title('(b) dft-abs')
 xlabel('f/Hz')
@@ -51,7 +51,7 @@ xlabel('f/Hz')
 fn_abs1=abs(y1/N1);
 fn_abs1(1)=fn_abs1(1)/2;
 subplot(223)
-bar(f(1:N),fn_abs(1:N),0.2)
+bar(f(1:N),fnAbs(1:N),0.2)
 axis([-1 df*N 0 4])
 title('(c) fn-abs')
 xlabel('f/Hz')
@@ -65,7 +65,7 @@ figure
 fnd_abs1=abs(dt*y1);
 fnd_abs1(1)=fnd_abs1(1)/2;
 subplot(221)
-bar(f(1:N),fnd_abs(1:N),0.2)
+bar(f(1:N),fndAbs(1:N),0.2)
 axis([-1 df*N 0 200])
 title('(d) fnd-abs')
 xlabel('f/Hz')
@@ -98,12 +98,12 @@ t=dt*(1:N);
 df=1/(N*dt);
 f=df*(1:N)-df;
 y=fft(x);
-dft_abs=abs(y);
-dft_abs(1)=dft_abs(1)/2;
-fn_abs=abs(y/N);
-fn_abs(1)=fn_abs(1)/2;
-fnd_abs=abs(dt*y);
-fnd_abs(1)=fnd_abs(1)/2;
+dftAbs=abs(y);
+dftAbs(1)=dftAbs(1)/2;
+fnAbs=abs(y/N);
+fnAbs(1)=fnAbs(1)/2;
+fndAbs=abs(dt*y);
+fndAbs(1)=fndAbs(1)/2;
 an=2*abs(y)/N;
 an(1)=an(1)/2;
 t1=dt*(1:N1);
@@ -120,7 +120,7 @@ y1=fft(x1);
 dft_abs1=abs(y1);
 dft_abs1(1)=dft_abs1(1)/2;
 subplot(221)
-bar(f(1:N),dft_abs(1:N),0.2)
+bar(f(1:N),dftAbs(1:N),0.2)
 %axis([-1 df*N 0 5000])
 axis([-1 df*N 0 8000000])
 title('(b) dft-abs')
@@ -135,7 +135,7 @@ xlabel('f/Hz')
 fn_abs1=abs(y1/N1);
 fn_abs1(1)=fn_abs1(1)/2;
 subplot(223)
-bar(f(1:N),fn_abs(1:N),0.2)
+bar(f(1:N),fnAbs(1:N),0.2)
 axis([-1 df*N 0 1200])
 title('(c) fn-abs')
 xlabel('f/Hz')
@@ -149,7 +149,7 @@ figure
 fnd_abs1=abs(dt*y1);
 fnd_abs1(1)=fnd_abs1(1)/2;
 subplot(221)
-bar(f(1:N),fnd_abs(1:N),0.2)
+bar(f(1:N),fndAbs(1:N),0.2)
 axis([-1 df*N 0 200000])
 title('(d) fnd-abs')
 xlabel('f/Hz')

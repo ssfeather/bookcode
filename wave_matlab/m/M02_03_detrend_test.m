732bc6a4d414934920a0434bfae9ed4a4b8c6ab5
muly <sssfeather@gmail.com>
2016-12-29 14:33:31 +0800

    ""

--- wave_matlab/m/M02_03_detrend_test.m
+++ wave_matlab/m/M02_03_detrend_test.m
@@ -1,17 +1,15 @@
-% 去掉分段线性趋势实验
-% sigData：无线性趋势的信号
-% 
-% treData：两段线性趋势
-% 
-% mixData：信号数据+趋势数据
+%% 去掉分段线性趋势实验
+%       sigData：无线性趋势的信号
+%       treData：两段线性趋势
+%       mixData：信号数据+趋势数据
 
 sigData = [0 1 -2 1 0 1 -2 1 0 0 1 -2 1 0 1 -2 1 0]; 
 treData = [0 1 2 3 4 3 2 1 0 0 1 2 3 4 3 2 1 0];      
 mixData = sigData+treData;  
-%断点在第[5 9 10 14]个元素
+%--断点在第[5 9 10 14]个元素-------------------------
 detData = detrend(mixData,'linear',[5 9 10 14]); 
 
-figure()
+figure();
 mp = plot(mixData,'b- .');
 hold on
 dp = plot(detData,'r- *');

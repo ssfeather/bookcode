8acaa755e903d150c1935449c7aae7f7c4687b79
muly <sssfeather@gmail.com>
2016-12-29 14:31:16 +0800

    ""

--- wave_matlab/m/M02_02_demmean_test.m
+++ wave_matlab/m/M02_02_demmean_test.m
@@ -1,11 +1,14 @@
 %% 波形数据去滑动平均值试验
+clearvars;
 %% 1.加载实验波形数据
 % 原始波形数据为：1999年11月29日|岫岩地震主震，辽宁地震台网大连台北南向记录|
 
-clear all;
 oriData = load('M02_02_dalianN1.dat');
 datLength= length(oriData);
-figure();
+
+scrsz = get(groot,'ScreenSize');
+figure('Position',[scrsz(3)/3 scrsz(4)/2 scrsz(3)/2 scrsz(4)/1.2]);
+subplot(4,1,1)
 plot(oriData);
 title('原始数据波形');
 %% 2.原始数据中加入低频正弦噪声
@@ -13,7 +16,8 @@ title('原始数据波形');
 for i=1:datLength
     mixData(i) = oriData(i)+20000*sin(2*pi*i/5000); %低频正弦信号：20000*sin(2*pi*i/5000
 end
-figure(1);
+
+subplot(4,1,2);
 plot(mixData);
 title('加上低频正弦波噪声后的数据波形');
 %% 3.使用去滑动均值法，去除低频正弦波噪声
@@ -27,10 +31,11 @@ title('加上低频正弦波噪声后的数据波形');
 
 sliLength=200;
 [demData,meaData]=M02_02_demmean_func(mixData,sliLength);
-figure(2);
+
+subplot(4,1,3)
 plot(demData);
 title('去除低频正弦波噪声后的数据波形');
-figure(3);
+subplot(4,1,4)
 plot(meaData);
 title('低频正弦噪声波形');
 %% 4.计算加入低频正弦波信号的波形数据振幅谱
@@ -39,7 +44,10 @@ Fs=50; %采样率
 NFFT = 2^nextpow2(datLength); 
 Y= fft(mixData,NFFT)/datLength;
 f = Fs/2*linspace(0,1,NFFT/2+1);
-figure(4);
+
+scrsz = get(groot,'ScreenSize');
+figure('Position',[scrsz(3)/3 scrsz(4)/2 scrsz(3)/1.5 scrsz(4)/2]);
+subplot(1,2,1);
 plot(f,2*abs(Y(1:NFFT/2+1))) ;
 xlabel('频率/Hz');
 ylabel('|Y(f)|');
@@ -50,7 +58,7 @@ Fs=50; %采样率
 NFFT = 2^nextpow2(datLength); 
 Y = fft(demData,NFFT)/datLength;
 f = Fs/2*linspace(0,1,NFFT/2+1);
-figure(5);
+subplot(1,2,2)
 plot(f,2*abs(Y(1:NFFT/2+1))) ;
 xlabel('频率/Hz');
 ylabel('|Y(f)|');

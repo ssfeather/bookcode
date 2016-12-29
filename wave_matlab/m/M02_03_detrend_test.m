2d3c45aa83212a0b078d0b04195b701816c417ee
muly <sssfeather@gmail.com>
2016-12-29 14:12:05 +0800

    ""

--- wave_matlab/m/M02_03_detrend_test.m
+++ wave_matlab/m/M02_03_detrend_test.m
@@ -1,12 +1,24 @@
-%  去掉分段线性趋势
-sig = [0 1 -2 1 0 1 -2 1 0 0 1 -2 1 0 1 -2 1 0];      % 无线性趋势的信号
-trend = [0 1 2 3 4 3 2 1 0 0 1 2 3 4 3 2 1 0];      % 两段线性趋势
-x = sig+trend;                    % 信号+趋势
-y = detrend(x,'linear',[5 9 10 14])         % 断点在第5个元素
-plot(x,'b- .');
+% 去掉分段线性趋势实验
+% sigData：无线性趋势的信号
+% 
+% treData：两段线性趋势
+% 
+% mixData：信号数据+趋势数据
+
+sigData = [0 1 -2 1 0 1 -2 1 0 0 1 -2 1 0 1 -2 1 0]; 
+treData = [0 1 2 3 4 3 2 1 0 0 1 2 3 4 3 2 1 0];      
+mixData = sigData+treData;  
+%断点在第[5 9 10 14]个元素
+detData = detrend(mixData,'linear',[5 9 10 14]); 
+
+figure()
+mp = plot(mixData,'b- .');
 hold on
-plot(y,'r- *');
+dp = plot(detData,'r- *');
 hold on
-plot(trend,'g- +')
+tp = plot(treData,'g- +');
 title('去线性趋势示例')
 xlabel('数据点序号')
+legend([mp dp tp],'信号数据+趋势数据','去线性趋势数据','线性趋势数据',...
+                  'Location','southoutside','Orientation','horizontal');
+legend('boxoff')

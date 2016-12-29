d6f2680fd3a17dd318fb5d77d06c8efe8f092eeb
muly <sssfeather@gmail.com>
2016-12-29 14:14:10 +0800

    ""

--- wave_matlab/m/M02_02_demmean_test.m
+++ wave_matlab/m/M02_02_demmean_test.m
@@ -1,49 +1,57 @@
-%试验去滑动平均2013-01-28
-clear;
-load M02_02_dalianN1.dat;
-m=length(M02_02_dalianN1)
-x_n=M02_02_dalianN1(1:30000);
-for i=1:m
-    x(i)=x_n(i)+20000*sin(2*pi*i/5000);
+%% 波形数据去滑动平均值试验
+%% 1.加载实验波形数据
+% 原始波形数据为：1999年11月29日|岫岩地震主震，辽宁地震台网大连台北南向记录|
+
+clear all;
+oriData = load('M02_02_dalianN1.dat');
+datLength= length(oriData);
+figure();
+plot(oriData);
+title('原始数据波形');
+%% 2.原始数据中加入低频正弦噪声
+
+for i=1:datLength
+    mixData(i) = oriData(i)+20000*sin(2*pi*i/5000); %低频正弦信号：20000*sin(2*pi*i/5000
 end
 figure(1);
-title('原始波形，人为加上低频正弦波噪声')
-plot(x);
+plot(mixData);
+title('加上低频正弦波噪声后的数据波形');
+%% 3.使用去滑动均值法，去除低频正弦波噪声
+% 去除滑动段长为sliLength*2+1的滑动平均值
+% 
+%     sliLength：滑动段长度
+% 
+%     demData：去除低频正弦噪声后的波形数据
+% 
+%     meaData：低频正弦噪声数据
 
-M=200
-[y,m]=M02_02_demmean_func(x,M);
+sliLength=200;
+[demData,meaData]=M02_02_demmean_func(mixData,sliLength);
 figure(2);
-plot(y);
-title('去滑动平均结果，去掉了低频正弦波噪声')
+plot(demData);
+title('去除低频正弦波噪声后的数据波形');
 figure(3);
-plot(m);
-p=2*M+1;
-title({'滑动平均，结果只包含低频噪声，滑动窗长2M+1=',p});
+plot(meaData);
+title('低频正弦噪声波形');
+%% 4.计算加入低频正弦波信号的波形数据振幅谱
 
-figure(4)
-%绘出信号的频谱
-L=length(x)
-Fs=50 %采样率
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-Y = fft(x,NFFT)/L;
+Fs=50; %采样率
+NFFT = 2^nextpow2(datLength); 
+Y= fft(mixData,NFFT)/datLength;
 f = Fs/2*linspace(0,1,NFFT/2+1);
-%绘信号的振幅谱
-% Plot single-sided amplitude spectrum.
-plot(f,2*abs(Y(1:NFFT/2+1))) 
-title('原始带低频正弦波噪声的信号振幅谱')
-xlabel('频率/Hz')
-ylabel('|Y(f)|')
+figure(4);
+plot(f,2*abs(Y(1:NFFT/2+1))) ;
+xlabel('频率/Hz');
+ylabel('|Y(f)|');
+title('加入低频正弦波信号的波形数据振幅谱');
+%% 5.计算去除低频正弦波信号后的波形数据振幅谱
 
-figure(5)
-%绘出去滑动平均后的频谱
-L=length(x)
-Fs=50 %采样率
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-Y = fft(y,NFFT)/L;
+Fs=50; %采样率
+NFFT = 2^nextpow2(datLength); 
+Y = fft(demData,NFFT)/datLength;
 f = Fs/2*linspace(0,1,NFFT/2+1);
-
-% Plot single-sided amplitude spectrum.
-plot(f,2*abs(Y(1:NFFT/2+1))) 
-title('去掉低频噪声后的振幅谱')
-xlabel('频率/Hz')
-ylabel('|Y(f)|')
+figure(5);
+plot(f,2*abs(Y(1:NFFT/2+1))) ;
+xlabel('频率/Hz');
+ylabel('|Y(f)|');
+title('去除低频正弦波信号后的波形数据振幅谱');

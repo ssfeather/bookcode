9c7263306124f875729c00171d156d1017a0de7c
muly <sssfeather@gmail.com>
2016-12-29 14:15:53 +0800

    ""

--- wave_matlab/m/M02_02_demmean_resp.m
+++ wave_matlab/m/M02_02_demmean_resp.m
@@ -1,29 +1,29 @@
-clear
-%load firlh02.h %加载FIR滤波器系数
-%x=firlh02;
-%rate=x(1) %采样率
-%num=x(2)
-%for k=1:1:num
-%    y(k)=x(k+2);
-%end
-%滑动平均的等效fir滤波器
-M=200
-num=2*M+1
-rate=50
-for i=1:num
-    y(i)=1/num; %滑动平均的等效滤波器系数
+% 滑动平均的等效Fir滤波器
+
+clear all
+sliLength=200;
+datLength=2*sliLength+1;
+Fs=50;
+filCloth = zeros(1,datLength);
+for i=1:datLength
+    filCloth(i)=1/datLength; %滑动平均的等效滤波器系数
 end
-[ B,theta,f1] = M02_02_firresp_func(y,num,rate );
+[ B,theta,f1] = M02_02_firresp_func(filCloth,datLength,Fs);
+p=unwrap(theta*pi/180)*180/pi;
+
 figure()
-subplot(2,1,1);
-% plot(f1,A);
+subplot(3,1,1);
 plot(f1,B);
 xlabel('频率/Hz');
 ylabel('振幅响应/dB');
 title('滑动平均的振幅频率响应');
-subplot(2,1,2);
-p=unwrap(theta*pi/180)*180/pi;
+
+subplot(3,1,2);
 plot(f1,p);
 xlabel('频率/Hz');
 ylabel('相位响应/度');
 title('滑动平均的相位频率响应')
+
+subplot(3,1,3);
+plot(filCloth,'.');
+title('滑动平均的等效滤波器系数')
